2901490a0d50a171332ae7f0e61994e5b26b18e0
Matheus Ferrarezi <matheus.fe.m@gmail.com>
2023-05-31 22:06:17 -0300

    Update lista4.js

--- lista4.js
+++ lista4.js
@@ -35,3 +35,41 @@ console.log(`Número de valores multiplos de 3 e 2 = ${vet23.length}, sendo eles
 console.log(`Número de valores multiplos de 2 = ${vet2.length}, sendo eles: ${vet2}`)
 console.log(`Número de valores multiplos de 3 = ${vet3.length}, sendo eles: ${vet3}`)   
 }
+
+function ex03(){
+                        //=-=-=PRIMEIRA PARTE=-=-=//
+    //DECLARAÇÃO DE VARIÁVEIS//
+    let vetcodigos=[]; let vetestoque=[];
+    //ENTRADA DE DADOS//
+    for(let i=5;i<5;i++){
+        vetcodigos[i]=Number(prompt(`Entre com o código do ${i+1}º produto: `))
+        vetestoque[i]=Number(prompt(`Informe a quantidade do produto ${i+1} em estoque:`))
+    }
+
+                        //=-=-=TERCEIRA PARTE=-=-=//
+    let cliente = Number(prompt("Informe o código do cliente."))
+    while(cliente!=0){
+
+                        //=-=-=SEGUNDA PARTE=-=-=//
+    //ENTRA COM O CÓDIGO E A QUANTIDADE DESEJADA//
+    let achou = false
+    let codigo=Number(prompt(`Informe o código do produto para a compra: `))
+    let quantidade=Number(prompt(`Informe a quantidade desejada do produto: `))
+    //VERIFICA SE O CÓDIGO EXISTE//
+    for(let i=0;i<5;i++){
+        if(codigo==vetcodigos[i]){
+            achou = true
+            if(vetcodigos>=quantidade){
+            vetestoque[i]=vetestoque-quantidade // ATUALIZA O ESTOQUE
+            }
+            else{
+                alert("Quantidade insuficiente.")
+            }
+        }
+        if(!achou){
+            alert("Produto não existe")
+        }
+    }
+    cliente = Number(prompt("Informe o código do cliente. Digite 0 para encerrar."))
+}
+}
