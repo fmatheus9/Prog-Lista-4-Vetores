c77a8bf38a0c9d39c32738b1ae12041f9f20241a
Matheus Ferrarezi <matheus.fe.m@gmail.com>
2023-06-05 22:16:09 -0300

    Update lista4.js

--- lista4.js
+++ lista4.js
@@ -68,8 +68,56 @@ function ex03(){
         }
         if(!achou){
             alert("Produto não existe")
+            
         }
     }
     cliente = Number(prompt("Informe o código do cliente. Digite 0 para encerrar."))
 }
+}
+
+
+//vetor.push(elemento)--> insere na ultima posição do vetor//
+//vetor.includes(elemento)--> verifica se o elemento esta contido no vetor//
+//vetor.indexOf(elemento)--> informa a posição do elemento dentro do vetor, caso
+//não encontrado informa o valor de -1
+
+function exe05(){
+    let vetlog=[]; let vetling=[]
+    for(let i=0;i<10;i++){
+        vetlog[i]=Number(prompt(`Informe o código do curso de lógica do aluno ${i+1}: `))
+    }
+    for(let i=0;i<10;i++){
+        vetling[i]=Number(prompt(`Informe o código do curso de linguagem do aluno ${i+1}: `))
+    }
+    // verificar alunos que fazem ambas as diciplinas
+    let comum=[]
+    for(let i=0;i<10;i++){
+        if(vetling.includes(vetlog[i])){ // se o vetlog[i] esta incluido no
+            //letling -> verifica se esta em linguagem tambem
+            comum.push(vetlog[i]) // adiciona o valor de vetlog[i] na ultima poição do comum
+    }
+    console.log(`Alunos que fazem ambas ${comum}`)
+}
+}
+
+function exe06(){
+    let salario = []; let nome = [] 
+    for(let i=0;i<5;i++){
+    nome[i] = Number(prompt(`Nome ${i+1}: `))
+    salario[i] = Number(prompt(`Salário: `))
+    }
+
+    let maior = salario[0]; let menor = salario[0]//assume que o menor valor seja o primeiro
+    for(let i=0;i<30;i++){
+        if(salario[i]>maior){
+            maior = salario[i]
+        }
+        if(salario[i]<menor){
+            menor = salario[i]
+        }
+    }
+    let nomemaior=nome(salario.indexOf(maior))
+    let nomemenor=nome(salario.indexOf(menor))
+    console.log(`${nomemaior} vai receber o salário ${maior}`)
+    console.log(`${nomemenor} vai receber o salário ${menor}`)
 }
